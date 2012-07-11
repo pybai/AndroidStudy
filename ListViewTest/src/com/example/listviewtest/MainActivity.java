d10fef0c8628d06f6d8d63ad63041de00f3ad210
baisubin <baisubin@jike.com>
2012-07-11 19:23:46 +0800

    2012-7-11

--- ListViewTest/src/com/example/listviewtest/MainActivity.java
+++ ListViewTest/src/com/example/listviewtest/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.listviewtest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 
-    @Override
+	private Button button1;
+	private Button button2;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        button1 = (Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
         setContentView(R.layout.activity_main);
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-
+	public void click1(){
+		startActivity(new Intent(MainActivity.this, MyListView1.class));
+	}
+	
+	public void click2(){
+		startActivity(new Intent(MainActivity.this,MyListView2.class));
+	}
     
 }
